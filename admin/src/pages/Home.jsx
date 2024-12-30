e533d2ddf6188fec25a262215664b3f857da8cb5
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2024-12-30 17:31:45 +0700

    feat: create component structure for App.jsx, install TailwindCSS for styling, and add Tailwind directives
    - Modularized App.jsx by creating components and pages structure.
    - Installed TailwindCSS to handle styling efficiently.
    - Added Tailwind directives to the CSS for proper styling setup.

---
+++ admin/src/pages/Home.jsx
@@ -0,0 +1,14 @@
+// import homeBg from '../assets/home-bg.jpg'
+
+const Home = () => {
+    return (
+        <div className='mt-10 mb-16 px-28 relative'>
+            {/* <img src="" src={homeBg} /> */}
+            <div className='bg-amber-800 text white rounded w-363 h-176 p-2 absolute top-250 left-900'></div>
+            <h2>selamat datang di HAR company</h2>
+            <h4>Disini admin dapat melakukan peng-inputan barang masul, dan barang keluar, <br /> serta juga dapat melakukan data permintaan barang setiap customer </h4>
+        </div>
+    )
+}
+
+export default Home
