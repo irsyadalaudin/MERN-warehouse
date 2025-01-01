d27979adad3d7609446c0bc9a2078ba1c7e9be98
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2025-01-01 10:59:53 +0700

    feat: move BrowserRouter to App.jsx and add Route/Routes for Home
    - Moved BrowserRouter from main.jsx to App.jsx for better component structure.
    - Added Route and Routes for Home in App.jsx to handle routing.

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
+import Storage from './components/Storage'
 import Home from './pages/Home'
 
 const App = () => {
 	return (
 		<>
-			<Navbar />
-			<Home />
+			<BrowserRouter>
+				<Navbar />
+				<Routes>
+					<Route path='/' element={<Home />} />
+				</Routes>
+				<Storage />
+			</BrowserRouter>
 		</>
 	)
 }
--- admin/src/main.jsx
+++ admin/src/main.jsx
@@ -1,14 +1,10 @@
 import { StrictMode } from 'react'
-// import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<BrowserRouter>
-		<StrictMode>
-			<App />
-		</StrictMode>
-	</BrowserRouter>
+	<StrictMode>
+		<App />
+	</StrictMode>
 )
