63ca559f2e52a544269597f9ae2fe2828ee686f5
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2025-01-08 16:17:57 +0700

    chore: improve styling for Navbar.jsx and remove unused link - Improved styling in Navbar.jsx for better design consistency. - Removed unused link to be reused later if necessary.

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,17 +1,26 @@
 import navLogo from '../assets/nav-logo2.png'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <nav className='flex flex-row items-center justify-between sticky top-0 z-10 px-28 bg-gray-300 shadow-lg'>
-            <div>
-                <img className='mt-1' src={navLogo} alt={'nav-logo'} />
+        <nav className='flex flex-row items-center justify-between sticky top-0 z-10 px-8 py-2 bg-gray-300 shadow-xl'>
+            <div className='flex items-center'>
+                <img className='w-16 h-16 object-contain' src={navLogo} alt='nav-logo' />
+                <span className='text-2xl font-bold ml-2'>My Warehouse</span>
             </div>
 
-            <div className='flex gap-4'>
-                <Link to='/' className='no-underline'>Beranda</Link>
-                <Link to='/' className='no-underline'>Penyimpanan barang</Link>
-            </div>
+            {/* <div className='md:flex gap-6'>
+                <Link
+                    to='/'
+                    className='text-black font-semibold hover:text-gray-200 transition-colors duration-300'>
+                    Home
+                </Link>
+                <Link
+                    to='/'
+                    className='text-black font-semibold hover:text-gray-200 transition-colors duration-300'>
+                    AddItem
+                </Link>
+            </div> */}
         </nav>
     )
 }
