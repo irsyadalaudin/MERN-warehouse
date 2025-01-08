1845729bb8a78a09053714ddaaf949ec650876f1
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2025-01-08 15:00:47 +0700

    chore: remove unused Home.jsx from App.jsx component, will reuse if necessary

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,17 +1,13 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import Warehouse from './components/Warehouse'
-import Home from './pages/Home'
 
 const App = () => {
 	return (
 		<>
 			<BrowserRouter>
 				<Navbar />
-				<Routes>
-					<Route path='/' element={<Home />} />
-				</Routes>
 				<Warehouse />
 			</BrowserRouter>
 		</>
