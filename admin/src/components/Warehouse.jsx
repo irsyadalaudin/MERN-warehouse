fd675bb432af4c2c5cbc1dfea12b40c643e886bb
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2025-02-16 21:21:29 +0700

    chore: remove unnecessary commented code for better readability and cleanliness

--- admin/src/components/Warehouse.jsx
+++ admin/src/components/Warehouse.jsx
@@ -14,30 +14,6 @@ const Warehouse = () => {
     const toggleDropdown = () => {
         setDropdownVisible(!dropdownVisible)
     }
-    /**
-    const sortItems = (items, option) => {
-        // IF NO SORT OPTION, RETURN AS-IS
-        if (!option) {
-            return items
-        }
-
-        return [...items].sort((a, b) => {
-            // SWITCH TO DETERMINE HOW TO SORT BASED ON THE PROVIDED OPTION
-            switch (option) {
-                case 'itemNameA-Z':
-                    return a.itemName.localeCompare(b.itemName)
-                case 'itemNameZ-A':
-                    return b.itemName.localeCompare(a.itemName)
-                case 'quantityAsc':
-                    return a.quantity-b.quantity
-                case 'quantityDesc':
-                    return b.quantity-a.quantity
-                // DEFAULT CASE WHEN NO MATCH IS FOUND, RETURN 0 (NO CHANGE IN ORDER)
-                default:
-                    return 0
-            }
-        })
-    }*/
 
     const sortedWarehouse = sortItems(warehouse, sortOption)
 
