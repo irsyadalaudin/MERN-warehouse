bd160e403978c295864809508b77c888c6c29826
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2025-02-19 00:33:32 +0700

    refactor: remove unnecessary `useEffect`, use `local useState-based` handleClickOutside function instead - wrap `sort button` and `dropdown` in a parent div with a single dropdownRef
    chore: comment out unnecessary `handleClickOutside useEffect`

--- admin/src/components/Warehouse.jsx
+++ admin/src/components/Warehouse.jsx
@@ -11,26 +11,39 @@ const Warehouse = () => {
     const [sortOption, setSortOption] = useState('itemNameA-Z')
     const [dropdownVisible, setDropdownVisible] = useState(false)
 
+    const dropdownRef = useRef()
+
     const toggleDropdown = () => {
         setDropdownVisible((prev) => !prev)
+        // e.stopPropagation()
+        // if (!dropdownVisible) {
+        //     setDropdownVisible(true)
+        // } else {
+        //     setDropdownVisible(false)
+        // }
+    }
+
+    const handleClickOutside = () => {
+        if (dropdownVisible) {
+            setDropdownVisible(false)
+        }
     }
 
     const sortedWarehouse = sortItems(warehouse, sortOption)
 
-    const dropdownRef = useRef()
 
-    useEffect(() => {
-        const handleClickOutside = (e) => {
-            if(dropdownRef.current && !dropdownRef.current.contains(e.target)) {
-                setDropdownVisible(false)
-            }
-        }
-        document.addEventListener('mousedown', handleClickOutside)
+    // useEffect(() => {
+    //     const handleClickOutside = (e) => {
+    //         if(dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+    //             setDropdownVisible(false)
+    //         }
+    //     }
+    //     document.addEventListener('mousedown', handleClickOutside)
 
-        return() => {
-            document.removeEventListener('mousedown', handleClickOutside)
-        }
-    }, [dropdownRef])
+    //     return() => {
+    //         document.removeEventListener('mousedown', handleClickOutside)
+    //     }
+    // }, [dropdownRef])
 
     useEffect(() => {
         const fetchWarehouseItem = async () => {
@@ -53,7 +66,7 @@ const Warehouse = () => {
     }, [])
 
     return (
-        <div className='flex flex-col lg:flex-row gap-8 p-8 min-h-screen'>
+        <div onClick={handleClickOutside} className='flex flex-col lg:flex-row gap-8 p-8 min-h-screen'>
             {/* ASIDE */}
             <aside className='w-full lg:w-1/4 bg-white p-6 rounded-xl shadow-lg'>
                 <h3 className='text-2xl font-bold mb-6 text-gray-900'>Manage Items</h3>
@@ -90,78 +103,81 @@ const Warehouse = () => {
             </aside>
 
             {/* MAIN */}
-            <div ref={dropdownRef} className='flex-1 lg:w-3/4'>
-                <div className='flex item-center justify-between'>
-                    <h2 className='text-3xl font-bold text-gray-900'>Warehouse Item</h2>
-                    <button
-                        type='button'
-                        onClick={toggleDropdown}
-                        className='mb-1 px-5 w-28 text-xl text-white font-bold rounded-lg shadow-lg bg-gradient-to-r from-cyan-600 to-cyan-400 hover:from-cyan-800 hover:to-cyan-600 transition-all disabled:opacity-50 disabled:text-gray-200'
-                        >
-                            Sort
-                    </button>
-                </div>
-
-                {dropdownVisible && (
-                    <div ref={dropdownRef} className='flex flex-col justify-self-end shadow-xl'>
+            <div /*ref={dropdownRef}*/ className='flex-1 lg:w-3/4'>
+                <div ref={dropdownRef} className='relative'>
+                    <div className='flex item-center justify-between'>
+                        <h2 className='text-3xl font-bold text-gray-900'>Warehouse Item</h2>
                         <button
+                            // ref={dropdownRef}
                             type='button'
-                            onClick={() => {
-                                setSortOption('itemNameA-Z')
-                                setDropdownVisible(false)
-                            }}
-                            className={`w-28 px-5 text-md text-white font-bold rounded-t-lg transition-all ${
-                                sortOption === 'itemNameA-Z'
-                                    ? 'bg-gradient-to-r from-cyan-800 to-cyan-600'
-                                    : 'bg-gradient-to-r from-cyan-600 to-cyan-400 hover:from-cyan-800 hover:to-cyan-600'
-                            }`}
+                            onClick={toggleDropdown}
+                            className='mb-1 px-5 w-28 text-xl text-white font-bold rounded-lg shadow-lg bg-gradient-to-r from-cyan-600 to-cyan-400 hover:from-cyan-800 hover:to-cyan-600 transition-all disabled:opacity-50 disabled:text-gray-200'
                             >
-                                A-Z
-                        </button>
-                        <button 
-                            type='button'
-                            onClick={() => {
-                                setSortOption('itemNameZ-A')
-                                setDropdownVisible(false)
-                            }}
-                            className={`w-28 px-5 text-md text-white font-bold transition-all ${
-                                sortOption === 'itemNameZ-A'
-                                    ? 'bg-gradient-to-r from-cyan-800 to-cyan-600'
-                                    : 'bg-gradient-to-r from-cyan-600 to-cyan-400 hover:from-cyan-800 hover:to-cyan-600'
-                            }`}
-                            >
-                                Z-A
-                        </button>
-                        <button 
-                            type='button'
-                            onClick={() => {
-                                setSortOption('quantityAsc')
-                                setDropdownVisible(false)
-                            }}
-                            className={`w-28 px-5 text-md text-white font-bold transition-all ${
-                                sortOption === 'quantityAsc'
-                                    ? 'bg-gradient-to-r from-cyan-800 to-cyan-600'
-                                    : 'bg-gradient-to-r from-cyan-600 to-cyan-400 hover:from-cyan-800 hover:to-cyan-600'
-                            }`}
-                            >
-                                0-1000
-                        </button>
-                        <button 
-                            type='button'
-                            onClick={() => {
-                                setSortOption('quantityDesc')
-                                setDropdownVisible(false)
-                            }}
-                            className={`w-28 px-5 text-md text-white font-bold rounded-b-lg transition-all ${
-                                sortOption === 'quantityDesc'
-                                    ? 'bg-gradient-to-r from-cyan-800 to-cyan-600'
-                                    : 'bg-gradient-to-r from-cyan-600 to-cyan-400 hover:from-cyan-800 hover:to-cyan-600'
-                            }`}
-                            >
-                                1000-0
+                                Sort
                         </button>
                     </div>
-                )}
+
+                    {dropdownVisible && (
+                        <div /*ref={dropdownRef}*/ className='flex flex-col justify-self-end shadow-xl'>
+                            <button
+                                type='button'
+                                onClick={() => {
+                                    setSortOption('itemNameA-Z')
+                                    setDropdownVisible(false)
+                                }}
+                                className={`w-28 px-5 text-md text-white font-bold rounded-t-lg transition-all ${
+                                    sortOption === 'itemNameA-Z'
+                                        ? 'bg-gradient-to-r from-cyan-800 to-cyan-600'
+                                        : 'bg-gradient-to-r from-cyan-600 to-cyan-400 hover:from-cyan-800 hover:to-cyan-600'
+                                }`}
+                                >
+                                    A-Z
+                            </button>
+                            <button 
+                                type='button'
+                                onClick={() => {
+                                    setSortOption('itemNameZ-A')
+                                    setDropdownVisible(false)
+                                }}
+                                className={`w-28 px-5 text-md text-white font-bold transition-all ${
+                                    sortOption === 'itemNameZ-A'
+                                        ? 'bg-gradient-to-r from-cyan-800 to-cyan-600'
+                                        : 'bg-gradient-to-r from-cyan-600 to-cyan-400 hover:from-cyan-800 hover:to-cyan-600'
+                                }`}
+                                >
+                                    Z-A
+                            </button>
+                            <button 
+                                type='button'
+                                onClick={() => {
+                                    setSortOption('quantityAsc')
+                                    setDropdownVisible(false)
+                                }}
+                                className={`w-28 px-5 text-md text-white font-bold transition-all ${
+                                    sortOption === 'quantityAsc'
+                                        ? 'bg-gradient-to-r from-cyan-800 to-cyan-600'
+                                        : 'bg-gradient-to-r from-cyan-600 to-cyan-400 hover:from-cyan-800 hover:to-cyan-600'
+                                }`}
+                                >
+                                    0-1000
+                            </button>
+                            <button 
+                                type='button'
+                                onClick={() => {
+                                    setSortOption('quantityDesc')
+                                    setDropdownVisible(false)
+                                }}
+                                className={`w-28 px-5 text-md text-white font-bold rounded-b-lg transition-all ${
+                                    sortOption === 'quantityDesc'
+                                        ? 'bg-gradient-to-r from-cyan-800 to-cyan-600'
+                                        : 'bg-gradient-to-r from-cyan-600 to-cyan-400 hover:from-cyan-800 hover:to-cyan-600'
+                                }`}
+                                >
+                                    1000-0
+                            </button>
+                        </div>
+                    )}
+                </div>
 
                 {!error ? (
                     <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6'>
