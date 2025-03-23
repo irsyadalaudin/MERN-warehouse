988a21c806971b85b4b9d68e58f77f7dae628ee8
irsyadalaudin491@gmail.com <irsyadalaudin491@gmail.com>
2025-03-24 04:31:19 +0700

    feat: ensure Cloudinary file ID matches MongoDB ID in updateWarehouse function - Update `updateWarehouse` to sync `public_id` and `url` from Cloudinary to MongoDB in `WarehouseControllers.js`

--- server/controllers/warehouseControllers.js
+++ server/controllers/warehouseControllers.js
@@ -37,7 +37,7 @@ const createWarehouse = async (req, res) => {
     // IF A FILE IS UPLOADED, SAVE ITS public_id AND url; OTHERWISE, SET FILE TO null
     try {
         let fileData = req.file
-            ? { public_id: req.file.public_id, url: req.file.url }  // public_id, url FROM CLOUDINARY 
+            ? { public_id: req.file.public_id, url: req.file.url }  // public_id AND url FROM CLOUDINARY 
             : null
         
         const warehouse = await Warehouse.create({ itemName, quantity, weight, weightDetails, file: fileData })
@@ -86,21 +86,27 @@ const updateWarehouse = async (req, res) => {
         return res.status(404).json({ error: 'no warehouse item matches the id!' })
     }
 
-    try {
+    // try {
         // CHECK IF THERE ARE NEW FILES UPLOADED, SAVE ITS path; OTHERWISE, SET FILE TO null
-        const file = req.file ? req.file.path : null
-
-        const warehouse = await Warehouse.findOneAndUpdate(
-            { _id: id },
-            {
-                ...req.body,
-                // IF THERE IS A NEW FILE, SET THE NEW FILE, OTHERWISE KEEP THE OLD FILE
-                // IF THE FILE IS NULL, UNDEFINED KEEPS THE OLD FILE IN THE DB
-                file: file || undefined
-            },
-            // TO RETURN THE LATEST DATA
-            { new: true }
-        )
+        // const file = req.file ? req.file.path : null
+        try {
+            let fileData = req.file
+                ? { public_id: req.file.public_id, url: req.file.url }  // public_id AND url FROM CLOUDINARY 
+                : null
+                
+            const warehouse = await Warehouse.findOneAndUpdate(
+                { _id: id },
+                {
+                    ...req.body,
+                    // IF THERE IS A NEW FILE, SET THE NEW FILE, OTHERWISE KEEP THE OLD FILE
+                    // IF THE FILE IS NULL, UNDEFINED KEEPS THE OLD FILE IN THE DB
+                    // file: file || undefined
+                    file: fileData || null
+                },
+                // TO RETURN THE LATEST DATA
+                { new: true }
+            )
+        // }
 
         if (!warehouse) {
             return res.status(400).json({ message: 'no such warehouse item has been updated!' })
